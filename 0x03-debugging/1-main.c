f4e180b83385d053599e374baf473cfa07cd815a
faysal1996git <faysalaqariden@gmail.com>
2023-06-21 14:19:37 -0700

    correction

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
 * main - causes an infinite loop
 * Return: 0
 */
@@ -15,10 +15,10 @@ i = 0;
 
 /*while (i < 10)*/
 /*{*/
-/*        putchar(i);*/
+/*putchar(i);*/
 /*}*/
-
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
 }
+
