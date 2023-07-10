732ee58b03fb640677d3f87b8ba3dce03ed2666d
faysal1996git <faysalaqariden@gmail.com>
2023-07-10 09:22:24 -0700

    tasks

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,29 +1,16 @@
 #include "main.h"
-
 #include <stdlib.h>
-
 /**
 * argstostr - main entry
-
 * @ac: int input
-
 * @av: double pointer array
-
 * Return: 0
-
 */
-
 char *argstostr(int ac, char **av)
-
 {
-
 int i, n, r = 0, l = 0;
-
 char *str;
-
-
 if (ac == 0 || av == NULL)
-
 return (NULL);
 for (i = 0; i < ac; i++)
 {
@@ -47,5 +34,4 @@ str[r++] = '\n';
 }
 }
 return (str);
-
 }
